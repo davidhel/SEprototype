7a80e4996cea59df698d77b10d13e629a3a1ca28
David Helgestad <dav-h@hotmail.com>
2014-03-12 13:01:49 +0100

    Endret layout

--- gen/no/gruppe3/seprototype/R.java
+++ gen/no/gruppe3/seprototype/R.java
@@ -14,9 +14,11 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
+        public static final int btnNewUser=0x7f060005;
         public static final int button1=0x7f060003;
         public static final int editText1=0x7f060001;
         public static final int editText2=0x7f060002;
+        public static final int tvNewUser=0x7f060004;
         public static final int txtWelcome=0x7f060000;
     }
     public static final class layout {
@@ -24,6 +26,8 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040000;
+        public static final int btnNewUser=0x7f040004;
+        public static final int newUser=0x7f040003;
         public static final int submit=0x7f040002;
         public static final int welcome=0x7f040001;
     }
