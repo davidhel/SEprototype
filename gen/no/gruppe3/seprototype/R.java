cfd91408ec9b6a8472aefd1301cc009b5c537f5c
David Helgestad <dav-h@hotmail.com>
2014-03-12 11:39:51 +0100

    opprettet activity for å logge inn

--- gen/no/gruppe3/seprototype/R.java
+++ gen/no/gruppe3/seprototype/R.java
@@ -13,8 +13,19 @@ public final class R {
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
+    public static final class id {
+        public static final int button1=0x7f060003;
+        public static final int editText1=0x7f060001;
+        public static final int editText2=0x7f060002;
+        public static final int txtWelcome=0x7f060000;
+    }
+    public static final class layout {
+        public static final int activity_main=0x7f030000;
+    }
     public static final class string {
-        public static final int app_name=0x7f030000;
+        public static final int app_name=0x7f040000;
+        public static final int submit=0x7f040002;
+        public static final int welcome=0x7f040001;
     }
     public static final class style {
         /** 
@@ -38,10 +49,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f040000;
+        public static final int AppBaseTheme=0x7f050000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f040001;
+        public static final int AppTheme=0x7f050001;
     }
 }
