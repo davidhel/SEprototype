b9cb5c2d9cbf91b96a76f3af75ea65180c1d46c5
David Helgestad <dav-h@hotmail.com>
2014-03-25 17:40:21 +0100

    Rettet alle feil

--- src/no/gruppe3/seprototype/MainActivity.java
+++ src/no/gruppe3/seprototype/MainActivity.java
@@ -1,29 +1,20 @@
 package no.gruppe3.seprototype;
 
-
-<<<<<<< HEAD
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-
-public class MainActivity extends FragmentActivity{
-
-=======
 import no.gruppe3.seprototype.R;
 import android.app.Activity;
-import android.content.Intent;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
-public class MainActivity extends Activity{
-	
+public class MainActivity extends Activity {
+
 	private String username;
 	private String password;
 	EditText txtPhone;
 	EditText txtPassword;
-	
->>>>>>> cb2346f428a71412c233008f43473d47f12da145
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
@@ -31,54 +22,49 @@ public class MainActivity extends Activity{
 		setContentView(R.layout.activity_main);
 		init();
 	}
-	
-	
+
 	private void init() {
 		username = null;
 		password = null;
 
 	}
 
-
-	public void btnSubmit(View view){
-		EditText txtPhone = (EditText)findViewById(R.id.etPhoneNumber);
-		EditText txtPassword = (EditText)findViewById(R.id.etPassword);
+	public void btnSubmit(View view) {
+		EditText txtPhone = (EditText) findViewById(R.id.etPhoneNumber);
+		EditText txtPassword = (EditText) findViewById(R.id.etPassword);
 		username = txtPhone.getText().toString();
 		password = txtPassword.getText().toString();
-		
-    	if(checkUsername(username)){
-			if(checkPassword(password)){
+
+		if (checkUsername(username)) {
+			if (checkPassword(password)) {
 				Intent intent = new Intent(this, OrderActivity.class);
 				startActivity(intent);
 			}
 		}
 
 	}
-	
-public  boolean checkUsername(String username) {
-		if(username.equals("1")){
+
+	public boolean checkUsername(String username) {
+		if (username.equals("1")) {
 			return true;
-		}
-		else{
+		} else {
 			return false;
 		}
 	}
-	
+
 	private boolean checkPassword(String password) {
-		if(password.equals("1")){
+		if (password.equals("1")) {
 			return true;
-		}
-		else{
+		} else {
 			return false;
 		}
-		
+
 	}
-<<<<<<< HEAD
-	public void btnRegister(View view){
+
+	public void btnRegister(View view) {
 		Intent intent = new Intent(this, RegisterUserActivity.class);
 		startActivity(intent);
 	}
-=======
 
 	@Override
 	protected void onResume() {
@@ -87,8 +73,4 @@ public  boolean checkUsername(String username) {
 		Intent intent = new Intent(this, ParkingRegisterActivity.class);
 		startActivity(intent);
 	}
-	
-	
->>>>>>> 466423fe548ddd2a1a610c5b347d19f49dcd4617
-	
 }
