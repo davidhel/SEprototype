b418153a8edc16a5c3e67e7b12c94a0f9a5f7b3c
davidhel <dav-h@hotmail.com>
2014-03-25 19:30:15 +0100

    Gjort ferdig layout på registrer bruker

--- src/no/gruppe3/seprototype/RegisterUserActivity.java
+++ src/no/gruppe3/seprototype/RegisterUserActivity.java
@@ -1,7 +1,9 @@
 package no.gruppe3.seprototype;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class RegisterUserActivity extends Activity{
 
@@ -9,7 +11,12 @@ public class RegisterUserActivity extends Activity{
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activitiy_register);
+		setContentView(R.layout.activitiy_register_user);
+	}
+	
+	public void btnSubmitUser(View view){
+		Intent intent = new Intent(this, MainActivity.class);
+		startActivity(intent);
 	}
 
 }
